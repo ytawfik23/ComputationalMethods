04987863c1f42c086cc2f345f9a741d81f674aef
Joseph F. Boudreau <boudreau@pitt.edu>
2026-09-18 10:22:52 -0400

    Fixed two mistakes:
      * multiplication by ln(2) rather than division.  (I also obtained
        ln2 from std::numbers, but there is nothing wrong with calling
        the function, either.
    
      * the matrix Lambda was missing an entry.

--- CH3/EX2/src/getFinalFraction.cpp
+++ CH3/EX2/src/getFinalFraction.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
+#include <numbers>
 #include <eigen3/Eigen/Dense>
 #include <eigen3/unsupported/Eigen/MatrixFunctions>
 
 Eigen::Vector3d getFinalFraction(Eigen::Vector3d initialFractions, double decayTime) {
-    double tau_b = 45.6 * log(2.0);
-    double tau_t = 132./60. * log(2.0);
-
+  using namespace std::numbers;
+  double tau_b = 45.6 / ln2;
+  double tau_t = 132./60. / ln2;
+  double tau_p = 195.0/ln2;
   Eigen::Matrix3d Lambda {
     {-1/tau_b, 0.0, 0.0},
     {1/tau_b, -1/tau_t, 0.0},
-    {0.0, 1/tau_t, 0.0}
+    {0.0, 1/tau_t,-1/tau_p}
   };
     Eigen::Vector3d finalFractions = (Lambda * decayTime).exp() * initialFractions;
     
